2053e8d554fe35cb88b7e50ab88e0d4446632f4e
Jonathan Reeve <jon.reeve@gmail.com>
2019-02-20 11:26:12 -0500

    annotate code, fix pandas build error

--- shell.nix
+++ shell.nix
@@ -1,14 +1,14 @@
 with import <nixpkgs> {};
 
 ((python37.withPackages (ps: with ps; [
-  pandas
+  pandas.override{doCheck = false;}
   matplotlib
   gensim
   spacy
   ftfy
   spacy_models.en_core_web_lg
   jupyter
-  scikitlearn
-  six
-  word2vec
+  # scikitlearn
+  # six
+  # word2vec
 ])).override({ignoreCollisions=true;})).env
